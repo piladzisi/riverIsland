2a8fd9aa2213ba376082c90258116bf7169a901b
Anna Sibirtseva <anna.sibirtseva@contractors.roche.com>
2020-08-31 16:38:19 +0300

    Initial Commit

---
+++ RiverIsland/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiverIsland
+//
+//  Created by Anna Sibirtseva on 31/08/2020.
+//  Copyright © 2020 Anna Sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
